b313072552ea24ee7165594354a0291ee20d651c
root <root@ip-172-31-20-47.us-east-2.compute.internal>
2018-07-18 14:21:23 +0000

    modified:   projector.r

--- projector.r
+++ projector.r
@@ -24,29 +24,61 @@ getLikelihood=function(umitab,models,reg){
 }
 
 
-update_alpha_single_batch=function(umitab,models,noise_model,reg,max_noise_fraction=.75,max_ncells=5000){
+#update_alpha_single_batch=function(umitab,models,noise_model,reg,max_noise_fraction=.75,max_ncells=5000){
+#  if (ncol(umitab)>max_ncells){
+#    umitab=umitab[,sample(colnames(umitab),size = max_ncells)]   
+#  }
+# 
+#  if (nrow(models)!=length(noise_model)){
+#    stop("noise_models and models have different number of genes")
+#  }
+#  get_ll_a=function(alpha,models,noise_model,umitab,reg){
+#    adjusted_models=t((1-alpha)*t(models)+alpha*matrix(noise_model,ncol(models),nrow(models),byrow=T))
+#    ll_b=getLikelihood(umitab,adjusted_models,reg=reg)
+#    return(ll_b)
+#  }
+#  func_to_opt=function(x){
+#    message(round(x,digits=2))
+#    tot_ll=get_total_likelihood(get_ll_a(x,models,noise_model,umitab,reg))
+#    return(tot_ll)
+#  }
+#  
+#  optim_val=optimize(func_to_opt,c(1e-3,max_noise_fraction),maximum = T,tol=1e-3)
+#  
+#  return(optim_val$maximum)
+#}
+
+update_alpha_single_batch=function(umitab,models,noise_model,cell_to_cluster=NULL,reg,max_noise_fraction=.2,max_ncells=5000){
   if (ncol(umitab)>max_ncells){
     umitab=umitab[,sample(colnames(umitab),size = max_ncells)]   
   }
- 
+  
   if (nrow(models)!=length(noise_model)){
     stop("noise_models and models have different number of genes")
   }
-  get_ll_a=function(alpha,models,noise_model,umitab,reg){
+  
+  
+  get_tot_likelihood_fixed_clusters=function(alpha){
     adjusted_models=t((1-alpha)*t(models)+alpha*matrix(noise_model,ncol(models),nrow(models),byrow=T))
-    ll_b=getLikelihood(umitab,adjusted_models,reg=reg)
-    return(ll_b)
-  }
-  func_to_opt=function(x){
-#    message(round(x,digits=2))
-    tot_ll=get_total_likelihood(get_ll_a(x,models,noise_model,umitab,reg))
-    return(tot_ll)
+    ll=getLikelihood(umitab,adjusted_models,reg=reg)
+    return(mean(ll[cbind(1:nrow(ll),match(cell_to_cluster,colnames(ll)))]))
   }
   
-  optim_val=optimize(func_to_opt,c(1e-3,max_noise_fraction),maximum = T,tol=1e-3)
+  get_tot_likelihood=function(alpha){
+    adjusted_models=t((1-alpha)*t(models)+alpha*matrix(noise_model,ncol(models),nrow(models),byrow=T))
+    return(get_total_likelihood(getLikelihood(umitab,adjusted_models,reg=reg)))
+  }
   
+  if (!is.null(cell_to_cluster)){
+    optim_val=optimize(get_tot_likelihood_fixed_clusters,c(0,max_noise_fraction),maximum = T,tol=1e-3)
+  }
+  else{
+    optim_val=optimize(get_tot_likelihood,c(0,max_noise_fraction),maximum = T,tol=1e-3)
+  }
   return(optim_val$maximum)
 }
+
+ 
 
 
 getOneBatchCorrectedLikelihood=function(umitab,models,noise_model,alpha_noise=NULL,reg){
@@ -160,20 +192,22 @@ update_models_debatched=function(umis,cell_to_cluster,batch,noise_models,alpha_n
   clusters=unique(cell_to_cluster)
   clusters=as.character(clusters[order(as.numeric(clusters))])
   raw_counts=Matrix(0,nrow(umis),length(clusters),dimnames = list(rownames(umis),clusters))
-  tmp_raw_counts=t(aggregate(t(umis),cell_to_cluster))
+  tmp_raw_counts=t(aggregate.Matrix(t(umis),cell_to_cluster))
   raw_counts[match(rownames(tmp_raw_counts),rownames(raw_counts)),match(colnames(tmp_raw_counts),colnames(raw_counts))]=tmp_raw_counts
-  ag=aggregate(colSums(umis),by=list(batch,cell_to_cluster),sum)
-  numis_per_batch=sapply(split(colSums(umis),batch),sum)[colnames(noise_models)]
+  rm(tmp_raw_counts)
+  gc()
   numis_per_batch_cluster=matrix(0,ncol(noise_models),length(clusters),dimnames = list(colnames(noise_models),clusters))
   tmp_numis_per_batch_cluster=invisible(acast(data.frame(batch=batch,cluster=cell_to_cluster,numis=colSums(umis)),batch~cell_to_cluster,fun.aggregate=sum,value.var = "numis")[colnames(noise_models),colnames(raw_counts)])
   numis_per_batch_cluster[rownames(tmp_numis_per_batch_cluster),colnames(tmp_numis_per_batch_cluster)]=tmp_numis_per_batch_cluster
+  rm(tmp_numis_per_batch_cluster)
+  gc()
   expected_noise_counts=noise_models%*%(numis_per_batch_cluster*alpha_noise)
   adj_counts=pmax(raw_counts-expected_noise_counts,0)
   #i=14;plot(raw_counts[,i],expected_noise_counts[,i],log="xy");abline(0,1)
   #identify(raw_counts[,i],expected_noise_counts[,i],labels = rownames(raw_counts))
   #browser()  
-  models=t(t(adj_counts)/colSums(adj_counts))
-  return(as.matrix(models))
+  models=as.matrix(t(t(adj_counts)/colSums(adj_counts)))
+  return(models)
 }
 
 
