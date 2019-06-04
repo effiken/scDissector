fb3e752b36d5307411e225b7f80de242cae2912a
effiken <effiken@users.noreply.github.com>
2019-06-04 14:48:06 -0400

    git check

--- R/scPlots.R
+++ R/scPlots.R
@@ -71,6 +71,7 @@ plot_truth_heatmap=function(ds,cell_to_sample,cell_to_cluster,insamples,ingenes,
   score_genes=intersect(score_genes,rownames(ds))
   if ((!is.null(score_genes))&(length(score_genes)>1)){
     score_rank=rank(apply(log2(.1+as.matrix(ds[score_genes,,drop=F]))),2,mean)/ncol(ds)
+    
     if (reverse_score_order){
       score_rank=1-score_rank
     }
